aeda4f90667fad3b6ca5fb4527f7b15b87ff435d
XBai <xiaoBai@qq.com>
2024-01-11 10:10:48 +0800

    更新数据库字段3.0

--- src/main/java/com/atxbai/online/model/pojo/Project.java
+++ src/main/java/com/atxbai/online/model/pojo/Project.java
@@ -1,5 +1,6 @@
 package com.atxbai.online.model.pojo;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
@@ -13,11 +14,11 @@ import java.util.Date;
 @AllArgsConstructor
 public class Project {
 
-    @TableId("id")
-    private String id;
+    @TableId(value = "id",type = IdType.AUTO)
+    private Integer id;
 
     @TableField("tno")
-    private String tno;
+    private Integer tno;
 
     @TableField("mentor")
     private String mentor;
