06e9b1b1e57dc7f435a744ed8c699c361f6f3cc0
cino <1228420450@qq.com>
2024-01-14 14:36:19 +0800

    个人信息个人简历部分 jwt获取学号

--- src/main/java/com/atxbai/online/controller/StudentController.java
+++ src/main/java/com/atxbai/online/controller/StudentController.java
@@ -1,7 +1,9 @@
 package com.atxbai.online.controller;
 
+import com.atxbai.online.common.responseUtils.PageResponse;
 import com.atxbai.online.common.responseUtils.Response;
 import com.atxbai.online.common.securityUtils.JwtTokenHelper;
+import com.atxbai.online.model.pojo.DelieverResume;
 import com.atxbai.online.model.pojo.Resume;
 import com.atxbai.online.model.pojo.Student;
 import com.atxbai.online.service.ResumeService;
@@ -57,19 +59,22 @@ public class StudentController {
 
     @GetMapping("/selectById")
     @ApiOperation(value = "个人简历页面个人信息")
-    public Response selectById(@RequestHeader("UserToken")String UserToken){
-        String sno = jwtTokenHelper.getUsernameByToken(UserToken);
+    public Response selectById(@RequestHeader("Authorization") String userToken){
+        String token = StringUtils.substring(userToken, 7);
+        String sno = jwtTokenHelper.getUsernameByToken(token);
         Student student = studentService.selectBySno(sno);
         return Response.success(student);
     }
 
     @GetMapping("/StudentResume")
     @ApiOperation(value = "展示个人简历页面个人经历")
-    public Response StudentResume(@RequestHeader("UserToken")String UserToken){
-        String sno = jwtTokenHelper.getUsernameByToken(UserToken);
+    public Response StudentResume(@RequestHeader("Authorization") String userToken){
+        String token = StringUtils.substring(userToken, 7);
+        String sno = jwtTokenHelper.getUsernameByToken(token);
         Resume resume = resumeService.selectBySno(sno);
         return Response.success(resume);
     }
+
 
 
     @PutMapping("/editMessage")
@@ -80,7 +85,7 @@ public class StudentController {
     }
 
 
-    @PutMapping("/edit")
+    @PostMapping("/edit")
     @ApiOperation(value = "修改个人经历部分")
     public Response edit(@RequestBody Resume resume){
         resumeService.updateResume(resume);
